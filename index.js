4004e7fea6980f36e3faf06bdcb769bf9b9fcf20
Vaibhav Deep <70009406+vaibhavdeep319@users.noreply.github.com>
2021-07-12 17:22:54 +0530

    functionality improved

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+var p1 = "Player1";
+var p2 = "Player2";
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+function start(){
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+var randomNumber1=Math.floor(Math.random()*6+1);
+var randImg="images/dice" + randomNumber1 + ".png"
+document.querySelector(".img1").setAttribute("src",randImg);
 
+var randomNumber2=Math.floor(Math.random()*6+1);
+var randImg2="images/dice" + randomNumber2 + ".png"
+document.querySelector(".img2").setAttribute("src",randImg2);
 
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+// code to decide who wins
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="🚩 Player 1 Wins";
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent= "Player 2 Wins 🚩";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").textContent="Draw!";
+}
 }
